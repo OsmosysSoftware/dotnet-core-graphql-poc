88e499736f73ed0a7dbbc94bf6c184e0be5542e3
harish <harish@osmosys.co>
2024-02-12 12:06:31 +0530

    add graphql implementation

---
+++ graphql_demo/Repositories/TaskService.cs
@@ -0,0 +1,26 @@
+﻿using graphql_demo.Data;
+using graphql_demo.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace graphql_demo.Repositories
+{
+    public class TaskService : ITaskService
+    {
+        private readonly DbContextClass dbContextClass;
+
+        public TaskService(DbContextClass dbContextClass)
+        {
+            this.dbContextClass = dbContextClass;
+        }
+
+        public async Task<List<TaskDetails>> TaskListAsync()
+        {
+            return await dbContextClass.Tasks.ToListAsync();
+        }
+
+        public async Task<TaskDetails> GetTaskDetailByIdAsync(long taskId)
+        {
+            return await dbContextClass.Tasks.Where(ele => ele.TaskID == taskId).FirstOrDefaultAsync();
+        }
+    }
+}
